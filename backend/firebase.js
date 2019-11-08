7ff0c10fc3b87a1a577719258c88651a3aeea768
an <anle.hust@gmail.com>
2019-11-08 18:03:50 +0700

    a

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,14 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://bt1-firebase.firebaseio.com"
+});
+
 function initializeAppFunctions() {
-    process.env.GCLOUD_PROJECT = 'bt1-firebase';
+    process.env.GCLOUD_PROJECT = 'firestorebeta1test2';
     // [START initialize_app_functions]
     const functions = require('firebase-functions');
   
@@ -10,6 +19,22 @@ function initializeAppFunctions() {
     // [END initialize_app_functions]
     return db;
   }
+  function demoInitialize(db) {
+    // [START demo_initialize]
+    // Fetch data from Firestore
+    db.collection('cities').get()
+      .then(documentSet => {
+        // Print the ID and contents of each document
+        documentSet.forEach(doc => {
+          console.log(doc.id, ' => ', doc.data());
+        });
+      })
+      .catch(err => {
+        // Error fetching documents
+        console.log('Error', err);
+      });
+    // [END demo_initialize]
+  }
   function quickstartAddData(db) {
     // [START add_lovelace]
     let docRef = db.collection('users').doc('alovelace');
@@ -18,18 +43,4 @@ function initializeAppFunctions() {
       first: 'Ada',
       last: 'Lovelace',
       born: 1815
-    });
-    // [END add_lovelace]
-    function quickstartListen(db) {
-        // [START quickstart_listen]
-        db.collection('users').get()
-          .then((snapshot) => {
-            snapshot.forEach((doc) => {
-              console.log(doc.id, '=>', doc.data());
-            });
-          })
-          .catch((err) => {
-            console.log('Error getting documents', err);
-          });
-        // [END quickstart_listen]
-      }
+    });}
